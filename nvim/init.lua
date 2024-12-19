db0a92924b1f24c66629aae2975aadd93ca08e93
Thomas Magane <thomasmagane@duck.com>
2024-12-19 13:12:43 +0200

    neovim config: making changes to the plugins directory.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require("configs.options")
+require("configs.keymaps")
+require("configs.autocmds")
+
+require("configs.lazy")
+
+print("Configuration successsfully loaded.")
